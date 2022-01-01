6c66857a26ddaea6ff992206593fb8671e94a8ef
aref kebriaee <arefkebriaee@gmail.com>
2022-01-01 17:44:51 +0330

    send data by queryset in url

--- scraper.py
+++ scraper.py
@@ -1,8 +1,17 @@
 import requests
 
+# ---------------------------------------------------------------
+# send parameters to url.
+# we can send parameters inside request by 'params' argument.
+# for example we send 'car' as query to server.
+# params must be dictionery.
 
-url = 'http://www.webscrapingfordatascience.com/'
+url = 'http://www.webscrapingfordatascience.com/paramhttp/'
 
-my_request = requests.get(url)
+parameters = {
+    'query': 'car'
+}
+
+my_request = requests.get(url, params=parameters)
 
 print(my_request.text)
